c07719633d7e48c43c568ca17c7b5e0c405c863d
Null29 <muhamad.nasrul29@gmail.com>
2020-05-15 23:14:24 +0700

    Pembuatan kelas connect untuk melakukan koneksi dengan database.

---
+++ Logic/connect.java
@@ -0,0 +1,47 @@
+import  java.sql.*;
+//import  com.mysql.jdbc.*;
+class connect{
+    Connection conn;
+    connect() throws SQLException, ClassNotFoundException {
+        try{
+            conn = connectToDB();
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+    protected Connection connectToDB() throws SQLException, ClassNotFoundException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/tubes_pbo?serverTimezone=UTC&user=root&password=");
+            return conn;
+        }
+        catch(Exception e){
+            throw e;
+        }
+    }
+    protected PreparedStatement pst(String stm) throws SQLException, ClassNotFoundException {
+        try{
+            if(conn != null && !conn.isValid(0))
+                conn = connectToDB();
+            return conn.prepareStatement(stm);
+        }
+        catch(Exception e){
+            throw e;
+        }
+    }
+    protected  Statement st(String stm) throws SQLException, ClassNotFoundException {
+        try{
+            if(conn != null && !conn.isValid(0))
+                conn = connectToDB();
+            return conn.prepareStatement(stm);
+        }
+        catch(Exception e){
+            throw e;
+        }
+    }
+    public static void main(String[] args) {
+        connect tes;
+    }
+}
